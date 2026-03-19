c6f72805edb879437ac7c001b5fc35573a72ec50
pochat <48400262+pochat@users.noreply.github.com>
2026-03-19 14:14:18 -0700

    2nd commit

---
+++ Assignment 4/wordleGameLogic.js
@@ -0,0 +1,17 @@
+// Global variables
+
+const word = "APPLE"; // The word to guess
+const maxAttempts = 6; // Maximum number of attempts allowed
+const wordLength = 5; // Length of the word to guess
+let attempts = 0; // Current number of attempts
+let gameOver = false; // Flag to indicate if the game is over
+
+// Initialize on load
+
+window.onload = function() {
+    gameStarts();
+}
+
+function gameStarts() {
+
+}
