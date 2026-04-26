d75f8461bc2a68f9732989d5c9d804854e78ed8e
pochat <48400262+pochat@users.noreply.github.com>
2026-04-25 21:34:09 -0700

    first commit

---
+++ JSX_Guessing_Game/JSX_guessing_game/src/guess.jsx
@@ -0,0 +1,11 @@
+function MyApp() {
+    
+    return (
+        <>
+           <h1>Hi, I'm MyApp() exported</h1>
+           <p>from the file guess.jsx</p>
+        </>
+    )
+}
+
+export default MyApp
